e8d0713dec219a76675b0be6575bf0943473832e
Berend <berendvangaart@gmail.com>
2022-01-23 15:59:22 +0100

    feat: added code block functionality

--- src/components/parser/parser.tsx
+++ src/components/parser/parser.tsx
@@ -1,15 +1,36 @@
 import React, {FC} from 'react';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 type ParserProps = {
     markdown: string
 }
 
-const Parser: FC<ParserProps> = ({markdown }) => {
+const Parser: FC<ParserProps> = ({markdown}) => {
     return (
         <div>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}
+                           components={{
+                               code({node, inline, className, children, ...props}) {
+                                   const match = /language-(\w+)/.exec(className || '')
+                                   return !inline && match ? (
+                                       <SyntaxHighlighter
+                                           children={String(children).replace(/\n$/, '')}
+                                           language={match[1]}
+                                           PreTag="div"
+                                           style={vscDarkPlus}
+                                           {...props}
+                                       />
+                                   ) : (
+                                       <code className={className} {...props}>
+                                           {children}
+                                       </code>
+                                   )
+                               }
+                           }}
+            >{markdown}</ReactMarkdown>
         </div>
     );
 };
